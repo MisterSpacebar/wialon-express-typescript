d128990fb53f6bdbe87144a9f799b429fdfc27e2
MisterSpacebar <yibinwei@live.com>
2024-04-03 10:43:36 -0400

    setting up unit creation route, need to process data
    need to batch edit/update them with appropriate template properties

--- src/routes/register.ts
+++ src/routes/register.ts
@@ -1,22 +1,32 @@
 import express from 'express';
 import uploadService from '../services/uploadService';
+import updateService from '../services/updateService';
 
 const router = express.Router();
 
-router.post('/unit-upload', (req, res) => {
+router.post('/unit-upload', async (req, res) => {
   const { sessionId, names, imei, unitData } = req.body;
   // Assuming you have a service called "uploadService" that handles the upload to the API
   let processedDataArray: any[] = [];
+  let unitIDs: any[] = [];
   const sensors = unitData.sens;
   const hw_id = unitData.hw;
   const user_id = unitData.crt;
 
   let registerUnits = {
     names: names,
-    user_id: user_id
+    user_id: user_id,
+    hw_id: hw_id
   }
 
   //uploadService.uploadData(processedDataArray, sessionId);
+  try {
+    const unitResponse = await uploadService(registerUnits, sessionId);
+    console.log('Unit response:', unitResponse);
+    unitIDs = unitResponse.map((unit: any) => unit.item.id);
+  } catch (error) {
+    console.error('Failed to upload data:', error);
+  }
   
   // Process the received data here
   
---
+++ src/services/updateService.ts
@@ -0,0 +1,11 @@
+// src/services/updateService.ts
+
+const updateService = async (data: any, sessionId: any) => {
+
+    const url = `https://hst-api.wialon.us/wialon/ajax.html?svc=core/batch&sid=${sessionId}`;
+
+    // set up parameter array for the request
+    
+};
+
+export default updateService;
